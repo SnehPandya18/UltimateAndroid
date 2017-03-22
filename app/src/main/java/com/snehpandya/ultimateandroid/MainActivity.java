c5b4c2b5359c376e145cf8dc5306c3a57f9943b6
Sneh Pandya <sneh.pandya1@gmail.com>
2017-03-22 23:40:16 +0530

    Add Button & Toast

--- app/src/main/java/com/snehpandya/ultimateandroid/MainActivity.java
+++ app/src/main/java/com/snehpandya/ultimateandroid/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, ImageActivity.class);
                 startActivity(i);
+            }
+        });
+
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hey!", Toast.LENGTH_SHORT).show();
             }
         });
     }
