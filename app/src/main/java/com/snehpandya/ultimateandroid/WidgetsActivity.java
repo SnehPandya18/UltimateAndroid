0b4fa0671be464936f3aa8f72016e544b6b93c52
Sneh Pandya <sneh.pandya1@gmail.com>
2017-03-29 00:31:51 +0530

    Add: RadioGroup & RadioButton

--- app/src/main/java/com/snehpandya/ultimateandroid/WidgetsActivity.java
+++ app/src/main/java/com/snehpandya/ultimateandroid/WidgetsActivity.java
@@ -6,10 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class WidgetsActivity extends AppCompatActivity {
 
+    String message;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_widgets);
@@ -32,5 +37,53 @@ public class WidgetsActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    public void submit(View view) {
+
+        EditText editText = (EditText) findViewById(R.id.edittext);
+        String eText = editText.getText().toString();
+
+        RadioButton male = (RadioButton) findViewById(R.id.male);
+        boolean gender1 = male.isChecked();
+
+        RadioButton female = (RadioButton) findViewById(R.id.female);
+        boolean gender2 = female.isChecked();
+
+        int calculate = calculateAnswer(gender1, gender2);
+        String calculateSummary = createSummary(eText, calculate);
+        displayMessage(calculateSummary);
+    }
+
+    public int calculateAnswer(boolean gender1, boolean gender2) {
+        int gender = 0;
+
+        if (gender1) {
+            gender = 1;
+        } else if (gender2) {
+            gender = 2;
+        }
+
+        return gender;
+    }
+
+    public String createSummary(String name, int calculate) {
+        String messageIn = "Hello " + name + "!";
+        if (calculate == 1) {
+            messageIn += "\nYour gender is male.";
+        } else if (calculate == 2) {
+            messageIn += "\nYour gender is female.";
+        }
+        return messageIn;
+    }
+
+    public void displayMessage(String message) {
+        this.message = message;
+        TextView t = (TextView) findViewById(R.id.showTextView);
+        if (t != null) {
+            t.setText(message);
+        } else {
+            Toast.makeText(this, "Error!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
